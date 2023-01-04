0e6bfcc17087666073980547d6d4bed588ffa47f
Karina Fukuda <karina.fukuda@mercadolivre.com>
2023-01-04 01:58:48 -0300

    Indent and added comma

--- repository/AcademicFormationRepository.kt
+++ repository/AcademicFormationRepository.kt
@@ -15,7 +15,9 @@ class AcademicFormationRepository {
         subscribedOnCourses.add(academicFormation)
         println("Aluno(a) ${user.name} matriculado(a) na DIO com sucesso! A formação selecionada foi ${academicFormation.nameOfFormation}")
         academicFormation.contentsOfFormation.forEach {
-            println("Conteúdo ${academicFormation.contentsOfFormation.indexOf(it) + 1} desta formação: ${it.course} duração de ${it.duration} minutos e dificuldade ${it.level}")
+            println("Conteúdo ${academicFormation.contentsOfFormation.indexOf(it) + 1}  " +
+                    "desta formação: ${it.course}, duração de ${it.duration} " +
+                    "minutos e dificuldade ${it.level}")
         }
     }
 }
